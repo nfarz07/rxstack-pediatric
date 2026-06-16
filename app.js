95e9753c935273f611d4dfd504a9e5823b78f6aa
nfarz07 <64793240+nfarz07@users.noreply.github.com>
2026-06-16 13:15:23 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker
+      .register("./sw.js")
+
+      .then(reg => {
+        console.log("SW Registered");
+      })
+
+      .catch(err => {
+        console.log(err);
+      });
+
+  });
+
+}
